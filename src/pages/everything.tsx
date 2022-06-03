99e4122345928064d37b934e531ea1e5b95a2104
Ingun Jon <ingun37@gmail.com>
2022-06-03 10:12:57 +0900

    transparency

--- src/pages/everything.tsx
+++ src/pages/everything.tsx
@@ -4,7 +4,7 @@ import { three } from "../three";
 const id = "everything";
 export default function Everything() {
   useEffect(() => {
-    three(id, 512, 512);
+    three(id, 1024, 1024);
   }, []);
   return <div id={id} />;
 }
--- src/three.ts
+++ src/three.ts
@@ -1,9 +1,13 @@
 import {
+  AmbientLight,
+  DirectionalLight,
   Mesh,
   MeshBasicMaterial,
+  MeshPhongMaterial,
   PerspectiveCamera,
   Scene,
   TorusBufferGeometry,
+  TorusKnotBufferGeometry,
   WebGLRenderer,
 } from "three";
 
@@ -14,7 +18,18 @@ export function three(id: string, width: number, height: number) {
   const renderer = new WebGLRenderer();
   renderer.setSize(width, height);
   document.getElementById(id)!.appendChild(renderer.domElement);
-  scene.add(new Mesh(new TorusBufferGeometry(), new MeshBasicMaterial()));
+  scene.add(new DirectionalLight());
+  scene.add(new AmbientLight(undefined, 0.5));
+  scene.add(
+    new Mesh(
+      new TorusKnotBufferGeometry(),
+      new MeshPhongMaterial({
+        color: 0xfff000,
+        transparent: true,
+        opacity: 0.5,
+      })
+    )
+  );
   camera.position.z = 5;
   requestAnimationFrame(() => {
     renderer.render(scene, camera);
