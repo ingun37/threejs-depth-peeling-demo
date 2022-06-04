d4afb56641149cd3cc2828c1ad11bfbeb40d32ca
Ingun Jon <ingun37@gmail.com>
2022-06-04 15:47:34 +0900

    copyshader

--- src/three.ts
+++ src/three.ts
@@ -2,16 +2,22 @@ import {
   AmbientLight,
   DepthTexture,
   DirectionalLight,
+  Material,
   Mesh,
   MeshBasicMaterial,
+  MeshDepthMaterial,
   MeshPhongMaterial,
+  NoBlending,
   PerspectiveCamera,
   Scene,
   ShaderMaterial,
   TorusBufferGeometry,
   TorusKnotBufferGeometry,
   WebGLRenderer,
+  WebGLRenderTarget,
 } from "three";
+import { FullScreenQuad } from "three/examples/jsm/postprocessing/Pass";
+import { CopyShader } from "three/examples/jsm/shaders/CopyShader";
 
 export function three(id: string, width: number, height: number) {
   const scene = new Scene();
@@ -42,10 +48,32 @@ void main() {
     )
   );
   camera.position.z = 5;
-  const depthA = new DepthTexture(width, height);
-  const depthB = new DepthTexture(width, height);
 
+  const depthT = new DepthTexture(width, height);
+  const layer0 = new WebGLRenderTarget(width, height, {
+    depthTexture: depthT,
+  });
+  const cache = new Map<Mesh, number>();
+
+  const copy = new ShaderMaterial(CopyShader);
+  const quad = new FullScreenQuad(copy);
   requestAnimationFrame(() => {
+    scene.traverse((obj) => {
+      if (obj instanceof Mesh && obj.material instanceof Material) {
+        cache.set(obj, obj.material.blending);
+        obj.material.blending = NoBlending;
+      }
+    });
+    renderer.setRenderTarget(layer0);
     renderer.render(scene, camera);
+
+    renderer.setRenderTarget(null);
+    copy.uniforms.tDiffuse.value = layer0.texture;
+    quad.render(renderer);
+    scene.traverse((obj) => {
+      if (obj instanceof Mesh && obj.material instanceof Material)
+        obj.material.blending = cache.get(obj)!;
+    });
+    // renderer.render(scene, camera);
   });
 }
