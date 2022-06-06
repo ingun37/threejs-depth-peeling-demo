64b13af0b0aa00b27b15dfd8b74172a33daacc65
Ingun Jon <ingun37@gmail.com>
2022-06-06 19:36:15 +0900

    gatsby config siteUrl

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   pathPrefix: "/threejs-depth-peeling-demo",
 
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://ingun37.github.io/threejs-depth-peeling-demo/`,
   },
   plugins: [],
 };
